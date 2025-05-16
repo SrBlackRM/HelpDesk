d1dae3db6eb7b3e548f8b62ec6342a552ada0971
Luan Aniello <luananiellowork@gmail.com>
2025-05-16 14:43:58 -0300

    esqueci de enviar o sql

--- bd/table_creation.sql
+++ bd/table_creation.sql
@@ -14,41 +14,21 @@ CREATE TABLE Categories (
 );
 
 CREATE TABLE Users (
-    ID_User INT NOT NULL AUTO_INCREMENT,
-    User_Name VARCHAR(100) NOT NULL,
-    User_Email VARCHAR(150) NOT NULL UNIQUE,
-    User_Password VARCHAR(150) NOT NULL,
-    User_Phone VARCHAR(20),
-    User_Section VARCHAR(50),
-    User_Role ENUM('Cliente', 'Técnico', 'Administrador') NOT NULL DEFAULT 'Cliente',
-    User_Expertise VARCHAR(100),
-    User_Active BOOL NOT NULL DEFAULT FALSE,
-    
-    PRIMARY KEY (ID_User)
+    ID_User INT PRIMARY KEY AUTO_INCREMENT,
+    User_Email VARCHAR(255) UNIQUE NOT NULL,
+    User_Password VARCHAR(255) NOT NULL,
+    User_Role ENUM('user', 'adm') NOT NULL
 );
 
 CREATE TABLE Tickets (
-    ID_Ticket INT NOT NULL AUTO_INCREMENT,
-    Ticket_Opening_Data DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    Ticket_Closing_Data DATETIME,
-    Ticket_Status ENUM('Aberto', 'Em andamento', 'Fechado') NOT NULL DEFAULT 'Aberto',
-    Ticket_Priority ENUM('Baixa', 'Média', 'Alta') NOT NULL DEFAULT 'Média',
+    ID_Ticket INT PRIMARY KEY AUTO_INCREMENT,
+    Ticket_Title VARCHAR(255) NOT NULL,
     Ticket_Description TEXT NOT NULL,
-    Ticket_Title VARCHAR(250) NOT NULL,
-    ID_User_Technical INT, -- FK
-    ID_User_Requesting INT NOT NULL, -- FK
-    ID_Category INT NOT NULL,
-    
-    PRIMARY KEY (ID_Ticket),
-    
-    CONSTRAINT FK_UserTechnical FOREIGN KEY (ID_User_Technical)
-    REFERENCES Users(ID_User) ON DELETE CASCADE,
-    
-    CONSTRAINT FK_UserRequesting FOREIGN KEY (ID_User_Requesting)
-    REFERENCES Users(ID_User) ON DELETE CASCADE,
-    
-    CONSTRAINT FK_Category FOREIGN KEY (ID_Category)
-    REFERENCES Categories(ID_Category) ON DELETE CASCADE
+    Ticket_Category ENUM('Software', 'Hardware', 'Redes', 'Acesso') NOT NULL,
+    Ticket_Priority ENUM('baixa', 'media', 'alta') DEFAULT NULL,
+    ID_User INT NOT NULL,
+    Ticket_Status ENUM('aberto', 'fechado') NOT NULL DEFAULT 'aberto',
+    FOREIGN KEY (ID_User) REFERENCES Users(ID_User)
 );
 
 CREATE TABLE Interactions (
